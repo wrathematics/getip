f3546f442e90a229be822f01a80466c6498340dc
Drew Schmidt <wrathematics@gmail.com>
2017-02-13 15:48:39 -0500

    better naming shorthand in test

--- tests/validate.R
+++ tests/validate.R
@@ -1,37 +1,37 @@
-val <- getip:::validate_ipv4
+valid <- getip:::valididate_ipv4
 
 # should be true
 test <- "127.0.0.1"
-stopifnot(val(test))
+stopifnot(valid(test))
 
 test <- "8.8.8.8"
-stopifnot(val(test))
+stopifnot(valid(test))
 
 test <- "255.255.255.255"
-stopifnot(val(test))
+stopifnot(valid(test))
 
 
 # should be false
 test <- "11127.0.0.1"
-stopifnot(!val(test))
+stopifnot(!valid(test))
 
 test <- "8.8.8.8.8"
-stopifnot(!val(test))
+stopifnot(!valid(test))
 
 test <- "-127.0.0.1"
-stopifnot(!val(test))
+stopifnot(!valid(test))
 
 test <- "127.1"
-stopifnot(!val(test))
+stopifnot(!valid(test))
 
 test <- "256.255.255.255"
-stopifnot(!val(test))
+stopifnot(!valid(test))
 
 test <- "255.256.255.255"
-stopifnot(!val(test))
+stopifnot(!valid(test))
 
 test <- "255.255.256.255"
-stopifnot(!val(test))
+stopifnot(!valid(test))
 
 test <- "255.255.255.256"
-stopifnot(!val(test))
+stopifnot(!valid(test))
