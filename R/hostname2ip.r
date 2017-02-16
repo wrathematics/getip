29809d94df7637aa38cfeb48e468a8e9c0d2eafb
Drew Schmidt <wrathematics@gmail.com>
2017-02-16 17:12:06 -0500

    add hostname2ip() for linux

---
+++ R/hostname2ip.r
@@ -0,0 +1,26 @@
+#' hostname2ip
+#' 
+#' Converts a hostname (or vector of hostnames) to 
+#' 
+#' @details
+#' Only IPv4 addresses will be returned. If only IPv6 addresses exist (and if
+#' that is the case, allow me to be the first to greet you, person from the
+#' distant future) then \code{NA} is returned for that hostname.
+#' 
+#' @param hosts
+#' A vector of hostnames as strings (or just one).
+#' 
+#' @return
+#' A list of vectors of IP's.  Each IP is stored as a string.
+#' 
+#' @examples
+#' \dontrun{
+#' hosts <- c("www.google.com", "localhost", "www.yahoo.com")
+#' hostname2ip(hosts)
+#' }
+#' 
+#' @export
+hostname2ip <- function(hosts)
+{
+  .Call(R_hostname2ip, hosts)
+}
