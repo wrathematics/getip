6787b73ad5dd1bd0cb5a0e022b8d13a964b9f6c4
wrathematics <wrathematics@gmail.com>
2017-07-17 08:57:19 -0400

    rename test

--- tests/validate.r
+++ tests/validate.r
@@ -1,40 +1,49 @@
 ### Ensures that the internal ipv4 validation method operates correctly. Does
 ### not query any device or internet service (as getip() does).
 
-valid <- getip:::validate_ipv4
+valid = getip:::validate_ipv4
 
 # should be true
-test <- "127.0.0.1"
+test = "127.0.0.1"
 stopifnot(valid(test))
 
-test <- "8.8.8.8"
+test = "8.8.8.8"
 stopifnot(valid(test))
 
-test <- "255.255.255.255"
+test = "255.255.255.255"
 stopifnot(valid(test))
 
 
 # should be false
-test <- "11127.0.0.1"
+test = "11127.0.0.1"
 stopifnot(!valid(test))
 
-test <- "8.8.8.8.8"
+test = "8.8.8.8.8"
 stopifnot(!valid(test))
 
-test <- "-127.0.0.1"
+test = "-127.0.0.1"
 stopifnot(!valid(test))
 
-test <- "127.1"
+test = "127.1"
 stopifnot(!valid(test))
 
-test <- "256.255.255.255"
+test = "256.255.255.255"
 stopifnot(!valid(test))
 
-test <- "255.256.255.255"
+test = "255.256.255.255"
 stopifnot(!valid(test))
 
-test <- "255.255.256.255"
+test = "255.255.256.255"
 stopifnot(!valid(test))
 
-test <- "255.255.255.256"
+test = "255.255.255.256"
+stopifnot(!valid(test))
+
+test = c("255.255.255.255, 127.0.0.1")
+stopifnot(!valid(test))
+
+test = 127.001
+stopifnot(!valid(test))
+
+test = NA
 stopifnot(!valid(test))
